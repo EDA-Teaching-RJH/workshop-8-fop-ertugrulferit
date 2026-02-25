1eec5bbc128d670a77568d6436ba8c59a4b7fb4e
ertugrulferit <ef11@kent.ac.uk>
2026-02-25 11:51:01 +0000

    0

--- ___.py
+++ ___.py
@@ -1,8 +1,29 @@
-def main():
- x = int(input("What's x? "))
- print("x squared is", square(x))
+import re
+
+
 def square(n):
- return n * n
+    return n * n
+
+
+def hello(to="world"):
+    return f"hello, {to}"
+
+
+def is_valid_email(email):
+    # Returns True if it matches our final rule from Part 4, otherwise False
+    if re.search(r"^\w+@\w.+\.(ac.uk|gov.uk|nhs.net)$", email):
+        return True
+    return False
+
+
+def main():
+    email = input("What's your email? ").strip()
+    if is_valid_email(email):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
 if __name__ == "__main__":
- main()
+    main()
  
