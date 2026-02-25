8130e31ff2e05ae38086b041d69142123e97501a
ertugrulferit <ef11@kent.ac.uk>
2026-02-25 12:01:25 +0000

    iu[

--- Challenge task.py
+++ Challenge task.py
@@ -1,6 +1,7 @@
-from ___ import is_valid_email
+# Create a brand new script called read_contacts.py. Using csv.DictReader, write a short script that opens contacts.csv, reads the rows, and prints out a nicely formatted string for each person. Instead of referencing the array with numbers, we can use DictReader to address against the dictionary element
+import csv
 
-try:
- assert is_valid_email("sneaky@@kent.ac.uk") == False
-except AssertionError:
- print("Warning: A sneaky email bypassed the REGEX!")
+with open("contacts.csv") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(f"{row['name']} is {row['age']} years old and lives in {row['city']}.")
